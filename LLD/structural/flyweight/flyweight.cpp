04b34b205b6756501dde095d8279090b5715af46
User <palritika71@gmail.com>
2025-01-25 17:04:11 +0530

    flyweight code added

---
+++ LLD/structural/flyweight/flyweight.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <unordered_map>
+#include <memory>
+using namespace std;
+
+class Iletter {
+public:
+    virtual void display(int x, int y) = 0;
+    virtual ~Iletter() {};
+};
+
+class DocumentCharacter : public Iletter {
+    char letter;
+    string fontType;
+    int size;
+
+public:
+    DocumentCharacter(char letter, string fontType, int size) : letter(letter), fontType(fontType), size(size) {}
+
+    void display(int x, int y) override {
+        cout << "Letter '" << letter << "' at position (" << x << ", " << y << "), FontType: " << fontType << endl;
+    }
+};
+
+class CharacterFactory {
+    unordered_map<char, std::shared_ptr<Iletter>> charMap;
+
+public:
+    CharacterFactory() {
+        cout << "Char factory instance initialized" << endl;
+    }
+
+    std::shared_ptr<Iletter> createChar(char charValue, string fontType, int size) {
+        if (charMap.find(charValue) != charMap.end()) {
+            return charMap[charValue];
+        } else {
+            std::shared_ptr<Iletter> docChar = std::make_shared<DocumentCharacter>(charValue, fontType, size);
+            charMap[charValue] = docChar;
+            return docChar;
+        }
+    }
+};
+
+int main() {
+    CharacterFactory factory;
+    auto docChar = factory.createChar('t', "arial", 4);
+    
+
+    docChar->display(1, 2);
+    auto char2 = factory.createChar('t', "arial", 4);
+    char2->display(2,4);
+
+    return 0;
+}
